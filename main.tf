35f07bceb3ef69d98008c1f72a57977e737a5cd5
Clemens Wältken <clemens.waltken@microsoft.com>
2025-08-13 11:50:38 +0200

    Change virtual machine to use marketplace image

--- main.tf
+++ main.tf
@@ -3,7 +3,22 @@ provider "azurerm" {
 }
 
 variable "prefix" {
-  default = "bmw"
+  default     = "bmw"
+  description = "Prefix for all resources in this configuration."
+}
+
+variable "admin_username" {
+  default     = "azureuser"
+  description = "The username for the admin account on the VM."
+}
+
+locals {
+  vm_name = "${var.prefix}-vm"
+}
+
+resource "random_password" "admin_password" {
+  length  = 24
+  special = true
 }
 
 resource "azurerm_resource_group" "demo" {
@@ -35,11 +50,65 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.demo.name
 }
 
+resource "azurerm_network_security_group" "internal" {
+  name                = "${var.prefix}-internal-nsg"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "internal" {
+  subnet_id                 = azurerm_subnet.internal.id
+  network_security_group_id = azurerm_network_security_group.internal.id
+}
+
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  allocation_method   = "Static"
+  name                = "${var.prefix}-public-ip"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
 }
 
 resource "azurerm_network_interface" "main" {
@@ -51,31 +120,47 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
-resource "azurerm_virtual_machine" "ubuntu" {
-  name                  = "testbmw1234vm"
+resource "azurerm_marketplace_agreement" "audiocodes" {
+  publisher = "audiocodes"
+  offer     = "mediantsessionbordercontroller"
+  plan      = "mediantvesbcazure"
+}
+
+resource "azurerm_virtual_machine" "audiocodes" {
+  name                  = local.vm_name
   location              = azurerm_resource_group.demo.location
   resource_group_name   = azurerm_resource_group.demo.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
+  delete_os_disk_on_termination = true
+
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    publisher = "audiocodes"
+    offer     = "mediantsessionbordercontroller"
+    sku       = "mediantvesbcazure"
     version   = "latest"
   }
 
+  plan {
+    name      = "mediantvesbcazure"
+    product   = "mediantsessionbordercontroller"
+    publisher = "audiocodes"
+  }
+
+
   os_profile {
-    admin_username = "azureuser"
-    admin_password = "uiHi6er8halai0sieti5"
+    admin_username = var.admin_username
+    admin_password = random_password.admin_password.result
     computer_name  = "ubuntu"
   }
 
   storage_os_disk {
-    name          = "testbmw1234osdisk"
+    name          = "${local.vm_name}-osdisk"
     caching       = "ReadWrite"
     create_option = "FromImage"
   }
@@ -83,4 +168,11 @@ resource "azurerm_virtual_machine" "ubuntu" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
+  depends_on = [azurerm_marketplace_agreement.audiocodes]
+}
+
+output "initial_admin_password" {
+  sensitive = true
+  value     = random_password.admin_password.result
 }
